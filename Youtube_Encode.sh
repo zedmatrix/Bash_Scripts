1bfc04c6ef62fd25b1026fd04020cfa956e6ad1a
zedmatrix <zedmatrix007@gmail.com>
2024-12-20 22:14:12 -0800

    Update Youtube_Encode.sh

--- Youtube_Encode.sh
+++ Youtube_Encode.sh
@@ -13,27 +13,28 @@ printdash() { printf "${_yellow}"; printf "%.s=" {1..30}; printf "${_reset}\n"; 
 
 1080 () {
   crop=(crop=1920-70:1080-32:35:16)
-  scale=(scale=1920:1080:flags=lanczos,setsar=16/9,setdar=16/9,bwdif)
+  scale=(scale=1920:1080:flags=lanczos,setsar=16/9,setdar=16/9)
 }
-
 720 () {
   crop=(crop=1280-60:720-30:30:15)
-  scale=(scale=1280:720:flags=lanczos,setsar=16/9,setdar=16/9,bwdif)
+  scale=(scale=1280:720:flags=lanczos,setsar=16/9,setdar=16/9)
 }
-# Function Encode Requires $input $output $title
+# Function to give audio a boost 
 Encode () {
   log=(-loglevel error -stats)
   #1080
-  #vcodec=(-c:v hevc_nvenc -profile:v main10 -preset slow)
+  #vcodec=( -c:v hevc_nvenc -profile:v main10 -preset slow)
   #vbv=( -b:v 5M -maxrate 8M -bufsize 2M -r 60000/1001)
+  #deint=( bwdif or yadif )
   vcodec=(-c:v copy)
   acodec=(-c:a aac -b:a 192k -ac 2)
   loud=(loudnorm=I=-10:LRA=12:TP=-1.0)
   meta=(-metadata title="${title}")
   time ffmpeg "${log[@]}" -i "${input}" ${vcodec[@]} -af ${loud[@]} ${acodec[@]} "${meta[@]}" "${output}"
-  #time ffmpeg "${log[@]}" -i "${input}" -vf "${crop[@]},${scale[@]}" ${vcodec[@]} ${vbv[@]} -af ${loud[@]} ${acodec[@]} "${meta[@]}" "${output}"
-  #echo "${log[@]}" -i "${input}" -vf "${crop[@]},${scale[@]}" ${vcodec[@]} ${vbv[@]} -af ${loud[@]} ${acodec[@]} ${meta[@]} "${output}"
+  #time ffmpeg "${log[@]}" -i "${input}" -vf "${deint[@]},${crop[@]},${scale[@]}" ${vcodec[@]} ${vbv[@]} \
+  #-af ${loud[@]} ${acodec[@]} "${meta[@]}" "${output}"
 }
+# Convert input to output and title and gets duration just for infomation
 String () {
   channels=$(ffprobe -v error -select_streams a:0 -show_entries stream=channels -of csv=p=0 "$input")
   duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$input" | cut -d '.' -f 1)
@@ -44,8 +45,6 @@ String () {
   duration=$(printf "%02d:%02d:%02d" ${hour} ${mins} ${sec} )
 
   fields=$(echo "${filename}" | awk -F'-' '{printf ("%s %s %s",$1,$2,$3)}')
-  #fields=$(echo "${fields}" | sed -e 's/_Edit.mp4//')
-  #output=$(echo "${fields}" | sed -r -e 's/([ ])([0-9])/\1- \2/ ; s/_// ; s/_// ; s/_/- /3')
   title=$(echo "${fields} FFmpeg Hevc1080p" | sed -e 's/_//g')
   input="${path}/${filename}.${extension}"
   output=$(echo "${filename}" | sed 's/ \[[^]]*\]//')

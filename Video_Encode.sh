61737d094cbfac3ccac8daa3a1a4a1f23bb050a2
zedmatrix <zedmatrix007@gmail.com>
2024-12-20 22:10:52 -0800

    Update Video_Encode.sh

--- Video_Encode.sh
+++ Video_Encode.sh
@@ -20,11 +20,10 @@ printdash() { printf "${_yellow}"; printf "%.s=" {1..30}; printf "${_reset}\n"; 
   crop=(crop=1280-60:720-30:30:15)
   scale=(scale=1280:720:flags=lanczos,setsar=16/9,setdar=16/9)
 }
-# Function Encode Requires $input $output $title
+# Function to Encode Video and Audio
 Encode () {
   log=(-loglevel error -stats)
   1080
-  #vcodec=(-c:v libx265 -profile:v main)
   vcodec=(-c:v hevc_nvenc -profile:v main)
   vbv=( -b:v 8M -maxrate 10M -bufsize 2M -r 60000/1001)
   denoise=(hqdn3d=2:1:2:2)
@@ -35,9 +34,9 @@ Encode () {
   time ffmpeg "${log[@]}" -i "${input}" \
   -vf "${deint[@]},${denoise[@]},${crop[@]},${scale[@]}" ${vbv[@]} ${vcodec[@]} \
   -af ${loud[@]} ${acodec[@]} "${meta[@]}" "${output}"
-
-  #echo "${log[@]}" -i "${input}" -vf "${crop[@]},${scale[@]}" ${vcodec[@]} ${vbv[@]} -af ${loud[@]} ${acodec[@]} ${meta[@]} "${output}"
 }
+
+# Function to Get duration and create title and output
 String () {
   channels=$(ffprobe -v error -select_streams a:0 -show_entries stream=channels -of csv=p=0 "$input")
   duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$input" | cut -d '.' -f 1)
@@ -48,8 +47,6 @@ String () {
   duration=$(printf "%02d:%02d:%02d" ${hour} ${mins} ${sec} )
 
   fields=$(echo "${filename}" | awk -F'-' '{printf ("%s %s %s",$1,$2,$3)}')
-  #fields=$(echo "${fields}" | sed -e 's/_Edit.mp4//')
-  #output=$(echo "${fields}" | sed -r -e 's/([ ])([0-9])/\1- \2/ ; s/_// ; s/_// ; s/_/- /3')
   title=$(echo "${fields} FFmpeg Hevc1080p" | sed -e 's/_//g')
   input="${path}/${filename}.${extension}"
   output=$(echo "${filename}" | sed 's/ \[[^]]*\]//')

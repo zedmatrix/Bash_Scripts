f3b44269d8421b5506e909180814b18d518421d9
zedmatrix <zedmatrix007@gmail.com>
2025-01-12 07:05:44 -0800

    Update Profile_Addons.sh

--- Profile_Addons.sh
+++ Profile_Addons.sh
@@ -23,14 +23,30 @@ ListSort() {
 }
 # ie. Shuffle | sort > playlist
 Shuffle() {
-    find . -name "*.mp4" -type f | shuf -n 10
+    if [[ -z $1 ]]; then
+        find . -name "*.mp4" -type f | shuf -n 10
+    elif [[ $1 =~ ^[0-9]+$ ]]; then
+        find . -name "*.mp4" -type f | shuf -n "$1"
+    else
+        echo "Usage: Shuffle [number] > playlist.m3u"
+    fi
 }
-
+# PlayLise Creator sort forward or reverse by creation date
+PlayList() {
+    if [[ $1 == "r" ]]; then
+        find . -name "*.mp4" -type f -printf '%T@ %p\n' | sort -r | cut -d' ' -f2-
+    elif [[ $1 == "n" ]]; then
+        find . -name "*.mp4" -type f -printf '%T@ %p\n' | sort | cut -d' ' -f2-
+    else
+        echo "Usage: PlayList [r|n] > playlist.m3u"
+    fi
+}
+# Helpful Aliases
 alias GO='time sh Batch.sh'
-alias RADIO="mpv --volume=50 --loop-playlist --shuffle --playlist="
+alias RADIO="mpv --volume=60 --loop-playlist --shuffle --playlist="
 alias ls='ls --color=auto'
 alias ll='ls -l'
 
 export PATH=~/MyBin:$PATH
-export -f mcd WATCH YTD ListSort Shuffle
+export -f mcd WATCH YTD PlayList ListSort Shuffle
 # End of Profile_Addons.sh
